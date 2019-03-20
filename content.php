1df78f693f9be717d245abcb6e6d84e32e3ef7f9
dgar <daniel.garavaldi@bzz.ch>
2019-03-20 09:36:44 +0100

    Update links adjustment. JavaScript is now located in a the separate file adjust-links.js

--- content.php
+++ content.php
@@ -74,57 +74,8 @@ $content = new Content();
         }
     }
     ?>
+<script type="application/javascript" src="inc/script/adjust-links.js"></script>
 
-<script>
-//filter specific images whose path shouldn't be changed
-function filterImages(src){
-    //filter the following images
-    var filterList = ["zoom-in", "video", "red.ico", "yellow.ico", "draft.png"];
-    for (var i in filterList) {
-        if (src.includes(filterList[i])){
-            return true;
-        }
-    }
-    return false;
-}
-
-$(document).ready(function() {
-    var images = $("img");
-    $.each(images, function() {
-        var src = $(this).attr("src");
-        //Filter: check if specific images' paths shouldn't be changed
-        if (filterImages(src) === false) {
-            //if src does not contain the path-part data, then ...
-            if (!src.includes("data")) {
-                //src path should be changed with the lp (Lehrperson) key
-                var lp = $("#lp_key").val();
-                if (lp == "") {
-                    console.log("lp is empty!");
-                } else {
-                    $(this).attr("src", "data/" + lp + "/" + src);
-                    console.log("data/" + lp + "/" + src);
-                }
-            }
-        }
-    });
-
-    //select all anchor <a> in <figcaption> and change href accordingly
-    images = $("figcaption a");
-    $.each(images, function() {
-        var href = $(this).attr("href");
-        if (!href.includes("data")) {
-            var lp = $("#lp_key").val();
-            if (lp == "") {
-                console.log("lp is empty!");
-            } else {
-                $(this).attr("href", "data/" + lp + "/" + href);
-                console.log("data/" + lp + "/" + href);
-            }
-        }
-    });
-    //console.clear();
-});
-</script>
 </div> <!-- /container -->
 <?php include_once(__DIR__ ."/inc/footer.inc"); ?>
 
